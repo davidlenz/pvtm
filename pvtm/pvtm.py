221ce8cec7f500baa12121265f3712352c03494f
VikaNa <Viktoriia.Naboka@wirtschaft.uni-giessen.de>
2019-10-22 13:01:06 +0200

    renamed methods in .search_topic_by_term() and added assert message, checked the doc strings

--- pvtm/pvtm.py
+++ pvtm/pvtm.py
@@ -25,10 +25,9 @@ clean = lambda x: re.sub('\W+',' ', re.sub(" \d+", '',re.sub('[äöüß]','',str
 
 
 class Documents(object):
-    """
-
-    """
-
+    '''
+    :return: tagged documents.
+    '''
     def __init__(self, documents):
         self.documents = documents
         self.len = len(documents)
@@ -51,10 +50,10 @@ class PVTM(Documents):
         '''
         The function takes a list of texts and removes stopwords, special characters, punctuation as well
         as very frequent and very unfrequent words.
-        :param texts: original documents
+        :param texts: original documents.
         :param lemmatized: if lemmatized=False, lemmatization of the texts will be done.
-        :param kwargs: additional key word arguments passed to popularity_based_prefiltering() function
-        :return: cleaned texts
+        :param kwargs: additional key word arguments passed to popularity_based_prefiltering() function.
+        :return: cleaned texts.
         '''
 
         texts = [clean(x) for x in texts]
@@ -72,8 +71,8 @@ class PVTM(Documents):
         '''
         Vocabulary building using sklearn's tfidfVectorizer.
         :param data: a list of strings(documents)
-        :param min_df: words are ignored if the frequency is lower than min_df
-        :param max_df: words are ignored if the frequency is higher than min_df
+        :param min_df: words are ignored if the frequency is lower than min_df.
+        :param max_df: words are ignored if the frequency is higher than man_df.
         :return: corpus specific vocabulary
         '''
 
@@ -91,10 +90,10 @@ class PVTM(Documents):
         '''
         Prefiltering function which removes very rare/common words.
         :param data: a list of strings(documents)
-        :param min_df: words are ignored if the frequency is lower than min_df
-        :param max_df: words are ignored if the frequency is higher than max_df
+        :param min_df: words are ignored if the frequency is lower than min_df.
+        :param max_df: words are ignored if the frequency is higher than max_df.
         :param stopwords: a list of stopwords.
-        :return: filtered documents' texts and corpus specific vocabulary
+        :return: filtered documents' texts and corpus specific vocabulary.
         '''
         vocabulary = self.get_allowed_vocab(data, min_df=min_df, max_df=max_df)
         vocabulary = frozenset(vocabulary)
@@ -113,9 +112,9 @@ class PVTM(Documents):
     def lemmatize(self, texts, lang='en'):
         '''
         Lemmatization of input texts.
-        :param texts: original documents
-        :param lang: language of the text documents
-        :return: lemmmatized texts
+        :param texts: original documents.
+        :param lang: language of the text documents.
+        :return: lemmmatized texts.
         '''
         print('Start lemmatization...')
         t0 = time.time()
@@ -136,11 +135,11 @@ class PVTM(Documents):
 
     def fit(self, save=True, filename = 'pvtm_model', **kwargs):
         '''
-        First, a Doc2Vec model ist trained and clustering of the documents is done by means of
-        :param kwargs:
+        First, a Doc2Vec model is trained and clustering of the documents is done by means of GMM.
+        :param kwargs: additional arguments which should be passed to Doc2Vec and GMM.
         :param save: if you want to save the trained model set save=True.
-        :param filename: name of the saved model
-        :return: Doc2Vec model and GMM clusters
+        :param filename: name of the saved model.
+        :return: Doc2Vec model and GMM clusters.
         '''
         # generate doc2vec model
         self.model = gensim.models.Doc2Vec(self.x_docs, **{key: value for key, value in kwargs.items()
@@ -154,12 +153,10 @@ class PVTM(Documents):
 
         print('Start clustering..')
         self.gmm = mixture.GaussianMixture(**{key: value for key, value in kwargs.items()
-                                              if key in inspect.getfullargspec(
-                mixture.GaussianMixture).args})
+                                              if key in inspect.getfullargspec(mixture.GaussianMixture).args})
         print('Finished clustering.')
 
-        self.doc_vectors = np.array(self.model.docvecs.vectors_docs)  # ist dasselbe wie unten self.document_vectors
-
+        self.doc_vectors = np.array(self.model.docvecs.vectors_docs)
         self.gmm.fit(self.doc_vectors)
         self.BIC = self.gmm.bic(self.doc_vectors)
         self.cluster_center = self.gmm.means_
@@ -175,9 +172,9 @@ class PVTM(Documents):
         '''
         The function takes a string (document) and
         transforms it to vector using a trained model.
-        :param strings: new document string
+        :param strings: new document string.
         :param steps: number of times to train the new document.
-        :return: document vector
+        :return: document vector.
         '''
         return self.model.infer_vector(string.split(), steps=steps).reshape(1,-1)
 
@@ -185,16 +182,17 @@ class PVTM(Documents):
         '''
         The function takes a document vector
         and returns a distribution of a given document over all topics.
-        :param vector: document vector
-        :return: probability distribution of the vector over all topics
+        :param vector: document vector.
+        :return: probability distribution of the vector over all topics.
         '''
         return self.gmm.predict_proba(vector)
 
     def wordcloud_by_topic(self, topic, variant='sim', stop_words=None, n_words=100):
         '''
-        :param topic: number of a topic
-        :param n_words: number of words to be shown in a wordcloud
-        :return: a wordcloud with most common words
+        Create a wordcloud to the defined topic.
+        :param topic: number of a topic.
+        :param n_words: number of words to be shown in a wordcloud.
+        :return: a wordcloud with most common words.
         '''
         if variant == 'sim':
             text = self.top_topic_center_words.iloc[topic,:n_words]
@@ -210,27 +208,17 @@ class PVTM(Documents):
         ax.imshow(wordcloud, interpolation="bilinear", )
         ax.axis("off")
         return wordcloud
-        # text = pd.DataFrame(self.model.wv.similar_by_vector(self.cluster_center[topic],
-        #                                                    topn=100),
-        #                    columns=['word', "similarity"]).word.values
-        # text = ', '.join(text)
-        # wordcloud = WordCloud(max_font_size=50, max_words=n_words, stopwords=stopwords,
-        #                      background_color="white").generate(text)
-        # fig, ax = plt.subplots(figsize=(7, 4))
-        # ax.imshow(wordcloud, interpolation="bilinear", )
-        # ax.axis("off")
-        # plt.show()
 
 
     def get_document_topics(self):
         '''
+        Create data frame with the most frequent words per topic as well as the nearest words to the single topic centers.
         :return: self.topic_words data frame with 100 frequent words per topic and self.wordcloud_df containing all the
-        document texts assigned to the resulted topics
+        document texts assigned to the resulted topics.
         '''
         document_topics = np.array(self.gmm.predict_proba(np.array(self.model.docvecs.vectors_docs)))
         most_relevant_topic_per_document = pd.DataFrame(document_topics).idxmax(1)
         kk = pd.DataFrame(self.documents).join(pd.DataFrame(most_relevant_topic_per_document, columns=['top_topic']))
-        # kk[0] = kk[0].apply(lambda x : " ".join([word for word in x.split() if word.lower() not in stop_words]))
         self.wordcloud_df = kk.groupby('top_topic')[0].apply(list).apply(" ".join).str.lower()
         self.topic_words = pd.DataFrame(
             [pd.Series(self.wordcloud_df.loc[topic].split()).value_counts().iloc[:100].index.values for topic in
@@ -239,8 +227,8 @@ class PVTM(Documents):
     def most_similar_words_per_topic(self, topic, n_words):
         '''
         The function returns most similar words to the selected topic.
-        :param topic: topic number
-        :param n_words: number of words to be shown
+        :param topic: topic number.
+        :param n_words: number of words to be shown.
         :return: returns words which are most similar to the topic center as measured by cosine similarity.
         '''
         sims = cosine_similarity([self.cluster_center[topic]], self.model.wv.vectors)
@@ -248,7 +236,7 @@ class PVTM(Documents):
         text = [self.model.wv.index2word[k] for k in sims[:n_words]]
         return text
 
-    def search_topic_by_term(self, term, variant='sim', method=1, n_words=100):
+    def search_topic_by_term(self, term, variant='sim', method= 'combine', n_words=100):
         '''
         This function returns topic number and a wordcloud which represent defined search terms.
         :param term: a list with search terms, e.g. ['market','money']
@@ -256,14 +244,15 @@ class PVTM(Documents):
         if 'sim' is chosen the term is searched for among all the words that are nearest to the single topic centers.
         If 'count' is chosen only words from the texts which were assigned to the single topics are considered.
         :param method: if there are multiple search terms, three different methods can be used to find a representative
-        wordcloud. Method '1' considers all the search terms as a single text and the process is simple to assigning
-        new documents with .get_string_vector and .get_topic_weights. Method '2' also considers all the search terms as
+        wordcloud. Method 'combine' considers all the search terms as a single text and the process is simple to assigning
+        new documents with .get_string_vector and .get_topic_weights. Method 'sim_docs' also considers all the search terms as
         a single text and searches for the most similar documents and topics these similar documents were assigned to.
-        Third method transforms single search terms into word vectors and considers cosine similarity between each word
+        Third method (vec_sim) transforms single search terms into word vectors and considers cosine similarity between each word
         vector and topic centers.
         :param n_words: number of words to be shown in a wordcloud.
         :return: best matching topic and a wordcloud.
         '''
+        assert isinstance(term, (list, tuple)), 'term parameter should be a list or a tuple'
         if len(term) == 1:
             if variant == "sim":
                 matches = self.top_topic_center_words[self.top_topic_center_words == term[0]]
@@ -279,14 +268,14 @@ class PVTM(Documents):
 
         elif len(term) > 1:
 
-            if method == 1:
+            if method == 'combine':
                 string = ' '.join(term)
                 vector = np.array(self.get_string_vector(string))
                 best_matching_topic = self.gmm.predict(vector)[0]
                 print("best_matching_topic", best_matching_topic)
                 self.wordcloud_by_topic(best_matching_topic)
 
-            elif method == 2:
+            elif method == 'sim_docs':
                 string = ' '.join(term)
                 vector = np.array(self.get_string_vector(string))
                 docs_num = [self.model.docvecs.most_similar(positive=[np.array(vector).reshape(-1, )], topn=10)[i][0]
@@ -300,7 +289,7 @@ class PVTM(Documents):
                 print("best_matching_topic", best_matching_topic)
                 self.wordcloud_by_topic(best_matching_topic)
 
-            elif method == 3:
+            elif method == 'vec_sim':
                 vectors = [self.get_string_vector(t) for t in term]
                 terms_df = pd.DataFrame({'topic': range(self.gmm.n_components)})
                 for i in range(len(term)):
@@ -318,15 +307,20 @@ class PVTM(Documents):
     def infer_topics(self, text):
         """
         Infer topics from unseen documents.
-        :param text: array or list of strings
-        :return:
+        :param text: array or list of strings.
+        :return: probability distribution of the text vector over all topics.
         """
         vec = self.get_string_vector(text)
-        return self.get_topic_weights(vec.reshape(1,-1))
+        return self.get_topic_weights(vec)
 
-    def start_webapp(self, model_path=None, title="pvtm_webapp"):
+    def start_webapp(self, model_path = None, title = "pvtm_webapp"):
+        '''
+        Run the dash app for visualization of results.
+        :param model_path: path to the saved model.
+        :param title: title displayed above the CMD window.
+        :return: link to the running dash app.
+        '''
         import os
-
         if not model_path:
             tmp_save_path = 'pvtm_model_tmp'
         self.save(tmp_save_path)
@@ -334,11 +328,20 @@ class PVTM(Documents):
         os.system(f'start cmd.exe /c "TITLE {title}&& python webapp.py -m {tmp_save_path}"')
 
 
-    def stop_webapp(self, title):
+    def stop_webapp(self, title = "pvtm_webapp"):
+        '''
+        Stops running the dash app.
+        :param title: title displayed above the CMD window.
+        :return: stops running the dash app.
+        '''
         import os
         print(title)
         os.system(f'taskkill /fi "WindowTitle eq {title}"')
         print("done")
 
     def save(self, savepath):
+        '''
+        Save the trained model.
+        :param savepath: path the defined model should be stored in.
+        '''
         joblib.dump(self, savepath)
